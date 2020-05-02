0fa10792acb6ea2e661c49a30304ba67b6222c73
Fabrizio Silvestri <fabrizio.silvestri@yahoo.it>
2020-05-02 19:23:24 +0200

    add table points

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ import puma from '../images/sqs/puma.jpeg';
 import lemming from '../images/sqs/lemming.jpeg';
 import tigrotti from '../images/sqs/tigrotti.jpeg';
 
+import good from '../images/scores/good.png';
+import medium from '../images/scores/medium.png';
+import bad from '../images/scores/bad.png';
+
 import rocket from '../images/rocket.jpg';
 import space from '../images/space.jpg';
 import engine from '../images/engine.jpg';
@@ -36,6 +40,16 @@ const squadriglie = [
   'lemming',
   'tigrotti',
 ];
+
+const scoreIncarichi = [
+  ['02 Mag', '3', '1', '2', '1', '3', '3'],
+];
+
+const getColor = (score) => {
+  if (score > 2) return good;
+  if (score > 1) return medium;
+  return bad;
+}
 
 const incarichi = [
   '📰 italia',
@@ -101,12 +115,13 @@ const AppBottomLine = styled(AppLine)`
 
   background-image: url(${engine});
   background-position: center top;
-  background-repeat: norepeat;
+  background-repeat: repeat-x;
 `;
 
 const AppTopLine = styled(AppLine)`
   background-image: url(${top});
   background-position: center;
+  background-repeat: repeat-x;
 `;
 
 const AppSide = styled.div`
@@ -144,6 +159,8 @@ const AppContainer = styled.div`
 
 const Table = styled.div`
   width: calc(100% - 2rem);
+  max-width: 50rem;
+  margin: 1rem auto;
   border-radius: 20px;
 
   padding: 1rem;
@@ -151,43 +168,55 @@ const Table = styled.div`
   background-color: rgba(200, 200, 200, 0.7);
 `;
 
-const StyledCel = styled.span`
-  padding: 0.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const ScoreItem = styled.div`
+  width: 6rem;
+  margin: 1rem;
 
-  width: 12%;
-  margin: 1%;
-`;
-
-const StyledRow = styled.div`
-  margin: 0.1rem;
-  border: 1px black solid;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const StyledGrid = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  background-color: rgba(200, 200, 200, 0.5);
 
-  ${StyledRow}:nth-child(2n) {
+  span {
+    padding: 0.2rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  span:nth-child(1n) {
+    border-radius: 10px 10px 0 0;
     background-color: rgba(100, 100, 100, 0.3);
+  }
+  span:nth-child(2n) {
+    border-radius: 0 0 10px 10px;
+    background-color: rgba(200, 200, 200, 0.7);
+
+    img { width: 1rem; }
+  }
+`;
+
+const ScoreRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  margin: 1rem 0;
+  border: 2px grey solid;
+  border-radius: 15px;
+
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+  h4 {
+    width: 100%;
+    margin: 0px;
+    text-align: center;
   }
 `;
 
 const IncarichiSection = styled.section`
   width: calc(100% - 2rem);
   heigth: auto;
-  padding: 1rem;
 
   background-image: url(${space});
   background-position: center;
@@ -196,7 +225,7 @@ const IncarichiSection = styled.section`
   flex-direction: column;
   align-items: center;
 
-  h2 {
+  h1 {
     margin: 0.5rem;
     width: 100%;
     font-weight: bold;
@@ -204,7 +233,7 @@ const IncarichiSection = styled.section`
   }
 `;
 
-const Main = styled.main`
+const Draw = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -216,11 +245,18 @@ const Main = styled.main`
   background-color: black;
 `;
 
+const Main = styled.main`
+  width: 100vw;
+  height: auto;
+  margin: 0;
+  padding: 0;
+  background-color: black;
 
-const scoreIncarichi = [
-  ['05/02', '3', '3', '3', '3', '3', '3'],
-];
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 
 function App() {
@@ -229,49 +265,38 @@ function App() {
   const sortedSqs = squadriglie.slice(translateCoef, squadriglie.length).concat(squadriglie.slice(0, translateCoef));
   return (
     <Main>
-      <AppTopLine />
-      <AppMainLine>
-        <AppSide />
-        <AppContainer>
-          {incarichi.map((incarico, index) => (
-            <Item key={incarico}>
-              <h1>{incarico}</h1>
-              <ImgContainer>
-                <img src={logos[sortedSqs[index]]} alt={sortedSqs[index]} />
-              </ImgContainer>
-            </Item>
-          ))}
-        </AppContainer>
-        <AppSide />
-      </AppMainLine>
-      <AppBottomLine />
+      <Draw>
+        <AppTopLine />
+        <AppMainLine>
+          <AppSide />
+          <AppContainer>
+            {incarichi.map((incarico, index) => (
+              <Item key={incarico}>
+                <h1>{incarico}</h1>
+                <ImgContainer>
+                  <img src={logos[sortedSqs[index]]} alt={sortedSqs[index]} />
+                </ImgContainer>
+              </Item>
+            ))}
+          </AppContainer>
+          <AppSide />
+        </AppMainLine>
+        <AppBottomLine />
+      </Draw>
       <IncarichiSection>
         <Table>
-          <h2>Risultati incarichi</h2>
-          <StyledGrid>
-            <StyledGrid>
-              <StyledRow>
-                <StyledCel><strong>Data</strong></StyledCel>
-                <StyledCel><strong>Koala</strong></StyledCel>
-                <StyledCel><strong>Grizzly</strong></StyledCel>
-                <StyledCel><strong>Aironi</strong></StyledCel>
-                <StyledCel><strong>Puma</strong></StyledCel>
-                <StyledCel><strong>Lemming</strong></StyledCel>
-                <StyledCel><strong>Tigrotti</strong></StyledCel>
-              </StyledRow>
-              {scoreIncarichi.map((scores) => (
-                <StyledRow key={scores[0]}>
-                  <StyledCel><strong>{scores[0]}</strong></StyledCel>
-                  <StyledCel>{scores[1]}</StyledCel>
-                  <StyledCel>{scores[2]}</StyledCel>
-                  <StyledCel>{scores[3]}</StyledCel>
-                  <StyledCel>{scores[4]}</StyledCel>
-                  <StyledCel>{scores[5]}</StyledCel>
-                  <StyledCel>{scores[6]}</StyledCel>
-                </StyledRow>
+          <h1>Punteggi incarichi</h1>
+          {scoreIncarichi.map((scores, scoreIndex) => (
+            <ScoreRow>
+              <h4>Data {scores[0]}</h4>
+              {squadriglie.map((sq, index) => (
+                <ScoreItem>
+                  <span><strong>{sq}</strong></span>
+                  <span><img src={getColor(scores[index + 1])} alt={scores[index + 1]} /></span>
+                </ScoreItem>
               ))}
-            </StyledGrid>
-          </StyledGrid>
+            </ScoreRow>
+          ))}
         </Table>
       </IncarichiSection>
     </Main>
